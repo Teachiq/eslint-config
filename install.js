78e0ffb2c91b5de0553430824c619c8f3fb87d17
Vilberg <vilbergs@gmail.com>
2020-08-28 15:27:22 +0200

    One more level up

--- install.js
+++ install.js
@@ -7,7 +7,7 @@ const deps = Object.keys(package.peerDependencies)
 
 const install = () => {
     console.log('Installing peer dependencies...')
-    cp.exec(`npm i -D ${deps.join(' ')}`, { cwd: '../../' }, (err, stdout, stderr) => {
+    cp.exec(`npm i -D ${deps.join(' ')}`, { cwd: '../../../' }, (err, stdout, stderr) => {
         if (err) {
             console.error(`error installing peer dependencies: ${err}`)
         }
