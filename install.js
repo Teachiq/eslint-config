55d7c80ec7d7873c06fb62dd8ecb3bb5940a908e
Vilberg <vilbergs@gmail.com>
2020-08-28 16:11:28 +0200

    Simplify install script

--- install.js
+++ install.js
@@ -1,20 +1,10 @@
 const fs = require('fs')
-const cp = require('child_process')
+const install = require('spawn-npm-install')
 const path = require('path')
 
 const package = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json')))
 const deps = Object.keys(package.peerDependencies)
 
-const install = () => {
-    console.log('Installing peer dependencies...')
-    cp.exec(`npm i -D ${deps.join(' ')}`, { cwd: '../../../' }, (err, stdout, stderr) => {
-        if (err) {
-            console.error(`error installing peer dependencies: ${err}`)
-        }
+console.log('Installing peer dependencies...')
 
-        console.log(stdout)
-        console.log(stderr)
-    })
-}
-
-install()
+install(deps, { cwd: '../../../', stdio: 'inherit' })
