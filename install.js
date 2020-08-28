ed817c43b905430061e2d9eff32568fbfab7ce4e
Vilberg <vilbergs@gmail.com>
2020-08-28 15:15:33 +0200

    Hardcode path

--- install.js
+++ install.js
@@ -5,11 +5,9 @@ const path = require('path')
 const package = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json')))
 const deps = Object.keys(package.peerDependencies)
 
-console.log(process.cwd())
-
 const install = () => {
     console.log('Installing peer dependencies...')
-    cp.exec(`npm i -D ${deps.join(' ')}`, { cwd: process.cwd() }, (err, stdout, stderr) => {
+    cp.exec(`npm i -D ${deps.join(' ')}`, { cwd: '../../' }, (err, stdout, stderr) => {
         if (err) {
             console.error(`error installing peer dependencies: ${err}`)
         }
