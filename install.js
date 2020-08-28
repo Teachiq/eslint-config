b6b2d56e1ac63c831a28934fefc9911763ffbb42
Vilberg <vilbergs@gmail.com>
2020-08-28 14:53:51 +0200

    Make install script install att correct location

--- install.js
+++ install.js
@@ -1,14 +1,15 @@
 const fs = require('fs')
 const cp = require('child_process')
-const { stderr } = require('process')
+const path = require('path')
 
-const package = JSON.parse(fs.readFileSync('./package.json'))
-
+const package = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json')))
 const deps = Object.keys(package.peerDependencies)
+
+console.log(process.cwd())
 
 const install = () => {
     console.log('Installing peer dependencies...')
-    cp.exec(`npm i -D ${deps.join(' ')}`, (err, stdout, stderr) => {
+    cp.exec(`npm i -D ${deps.join(' ')}`, { cwd: process.cwd() }, (err, stdout, stderr) => {
         if (err) {
             console.error(`error installing peer dependencies: ${err}`)
         }
