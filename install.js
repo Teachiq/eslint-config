aef838faff983da684abe738ce28ee5f2c480103
Vilberg <vilbergs@gmail.com>
2020-08-31 16:00:21 +0200

    Upgrade install script to handle prettierrc config

--- install.js
+++ install.js
@@ -3,13 +3,45 @@
 const fs = require('fs')
 const spawn = require('child_process').spawn
 const path = require('path')
+const prettier = require('prettier')
 
 const package = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json')))
 const deps = Object.keys(package.peerDependencies)
 
-console.log('Installing peer dependencies...')
-npm = spawn('npm', ['install', '-D', ...deps], { stdio: 'inherit' })
+function installPeerDependencies() {
+  console.log('Installing peer dependencies...')
+  npm = spawn('npm', ['install', '-D', ...deps], { stdio: 'inherit' })
 
-npm.on('exit', () => {
-    process.exit(0)
+  return npm
+}
+
+async function createPrettierConfig() {
+  const internalDefaultConfig = {
+    semi: false,
+    singleQuote: true,
+    printWidth: 120,
+  }
+
+  const projectPrettierConfigPath = await prettier.resolveConfigFile()
+  const projectPrettierConfig = await prettier
+    .resolveConfig(projectPrettierConfigPath)
+    .then((config) => config)
+    .catch(() => ({}))
+
+  const newConfig = { ...projectPrettierConfig, ...internalDefaultConfig }
+
+  const fileToWriteTo = projectPrettierConfigPath
+    ? projectPrettierConfigPath
+    : path.join(process.cwd(), './.prettierrc')
+
+  fs.writeFileSync(fileToWriteTo, prettier.format(JSON.stringify(newConfig), { parser: 'json', printWidth: 50 }))
+}
+
+const install = installPeerDependencies()
+
+install.on('exit', async () => {
+  console.log('Generating prettier config...')
+  await createPrettierConfig()
+
+  process.exit(0)
 })
