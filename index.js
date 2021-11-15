d4e31cc01af547de09d9138861a59bc8e7e7992a
Christofer <christofer.wikman@exam.net>
2021-11-15 16:00:12 +0100

    2.0.1-beta.4

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
+/* Default rules live here */
+
 module.exports = {
-  root: true,
   env: {
     browser: true,
   },
   extends: [
-    'standard',
-    'plugin:vue/recommended',
+    'standard'
   ],
-  ignorePatterns: ['**/*.min.js', '**/polyfill.js'],
-  plugins: ['vue', 'file-progress'],
+  ignorePatterns: ['**/*.min.js', '**/polyfill.js', '**/*.d.ts'],
+  plugins: ['file-progress'],
   rules: {
     'file-progress/activate': 1,
-    'vue/static-class-names-order': 'warn',
-    'vue/no-unused-properties': 'warn',
-    'vue/max-len': [
-      'warn',
-      {
-        code: 120,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-        ignorePattern: 'd="([\\s\\S]*?)"', // Ignore SVG path
-      },
-    ],
-    'vue/multi-word-component-names': 'off',
-    'vue/attribute-hyphenation': 'off',
-    'vue/no-potential-component-option-typo': 'error',
-    'vue/html-quotes': ['error', 'double', { avoidEscape: true }],
     'space-before-function-paren': [
       'error',
       {
@@ -45,34 +28,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'generator-star-spacing': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-
   },
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      env: { browser: true, es6: true, node: true },
-      extends: [
-        'standard',
-        'plugin:vue/recommended',
-      ],
-      rules: {
-        'file-progress/activate': 1,
-        'space-before-function-paren': [
-          'error',
-          {
-            anonymous: 'always',
-            named: 'never',
-            asyncArrow: 'always',
-          },
-        ],
-        'comma-dangle': ['error', 'always-multiline'],
-      },
-    },
-    {
-      files: ['**/*.d.ts'],
-      rules: {
-        'no-useless-constructor': 'off',
-      },
-    },
-  ],
 }
---
+++ profile/typescript.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    '@teachiq',
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    '@typescript-eslint/no-unused-expressions': 1,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-extra-semi': 0,
+    '@typescript-eslint/no-unused-vars': [
+      1,
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+}
