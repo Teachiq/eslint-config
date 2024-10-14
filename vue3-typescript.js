e2e8b8bb918c47b617a827d9dd1e52c1866d460f
Edugolr <christofer.wikman@gmail.com>
2024-10-14 14:24:23 +0200

    4.0.0-alpha.5

--- vue3-typescript.js
+++ vue3-typescript.js
@@ -3,4 +3,4 @@ const defaultConfig = require('./configs/defaultConfig.js')
 const typescriptConfig = require('./configs/typescript.js')
 const vue3Config = require('./configs/vue3')
 
-module.exports = merge.all(defaultConfig, typescriptConfig, vue3Config)
+module.exports = merge.all([defaultConfig, typescriptConfig, vue3Config])
