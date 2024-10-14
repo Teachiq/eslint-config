58e3e0d32ac8c9ca22fb3471422296d3cfb342ac
Edugolr <christofer.wikman@gmail.com>
2024-10-14 14:19:15 +0200

    4.0.0-alpha.3

--- vue3-typescript.js
+++ vue3-typescript.js
@@ -1,6 +1,6 @@
-import { merge } from 'deepmerge'
-import defaultConfig from './configs/defaultConfig.js'
-import typescriptConfig from './configs/typescript.js'
-import vue3Config from './configs/vue3'
+const merge = require('deepmerge')
+const defaultConfig = require('./configs/defaultConfig.js')
+const typescriptConfig = require('./configs/typescript.js')
+const vue3Config = require('./configs/vue3')
 
-export default merge.all(defaultConfig, typescriptConfig, vue3Config)
+module.exports = merge.all(defaultConfig, typescriptConfig, vue3Config)
